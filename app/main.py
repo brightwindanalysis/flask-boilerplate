eb52d2c477ee8d3dfe2528b31037bae184d4d181
niqdev <niqdev@gmail.com>
2018-03-12 23:14:07 +0000

    init logs

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 #import app.api.example_api
 
+from app.utils.logger import Logger
+logger = Logger("logs/app.log")
+logger.init(app)
+app.logger.debug('A value for debugging')
+
 '''
 from utils.logger import Logger
 import api.example_api
--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
@@ -7,6 +8,8 @@ class Logger(object):
         self.path_logs = path_logs     
     
     def init(self, app):
+        os.makedirs(os.path.dirname(self.path_logs), exist_ok=True)
+
         formatter = logging.Formatter("[%(asctime)s] [%(pathname)s:%(lineno)d] %(levelname)s - %(message)s")
         handler = TimedRotatingFileHandler(self.path_logs, when='midnight', interval=1, backupCount=5)
         handler.setLevel(logging.DEBUG)
